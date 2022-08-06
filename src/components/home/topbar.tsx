55ce2627198d0bdc45b4986b0d603174416aff6a
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2022-08-07 01:03:44 +0300

    hook

--- src/components/home/topbar.tsx
+++ src/components/home/topbar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef } from "react";
 import Searchbox from "../global/searchbox";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown, faBell } from "@fortawesome/free-solid-svg-icons";
@@ -8,11 +8,13 @@ import { Avatar } from "../global/avatar";
 import { UserContext } from "../../context/UserContext";
 import { AnimatePresence, motion } from "framer-motion";
 import { faSignOut } from "@fortawesome/free-solid-svg-icons";
+import { useDetectOutsideClick } from "../../hooks/useDetectOutsideClick";
 
 const Topbar = () => {
   const { logout } = useLogin();
   const { user } = useContext(UserContext);
-  const [active, setActive] = useState(false);
+  const dropdownRef = useRef(null);
+  const [active, setActive] = useDetectOutsideClick(dropdownRef, false);
   return (
     <div className="topbar z-50  w-full gap-5 h-24 px-0 fixed right-0 top-0 flex items-center lg:gap-0 lg:pl-20 lg:pr-28 lg:justify-between">
       <div className="flex items-center w-full px-6 justify-between">
@@ -20,7 +22,10 @@ const Topbar = () => {
           <Searchbox />
         </div>
         <HiddenMenu />
-        <div className="flex gap-10 items-center relative cursor-pointer">
+        <div
+          ref={dropdownRef}
+          className="flex gap-10 items-center relative cursor-pointer"
+        >
           <div
             onClick={() => setActive(!active)}
             className="flex gap-3 items-center"
@@ -39,7 +44,7 @@ const Topbar = () => {
                 onClick={logout}
                 initial={{ opacity: 0, y: -10 }}
                 animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 10 }}
+                exit={{ opacity: 0, y: -10 }}
                 className="absolute bg-black text-white text-sm text-justify rounded w-36 h-auto p-2 top-[60px]"
               >
                 <ul>
